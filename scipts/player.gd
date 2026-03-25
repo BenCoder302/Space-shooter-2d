c1f723b877e322f34bdf7ff44fbda814ed53fe7d
BenCoder302 <bencoder302@gmail.com>
2026-03-25 15:33:33 +0530

    Primitive release

--- scipts/player.gd
+++ scipts/player.gd
@@ -1,4 +1,12 @@
 extends Node2D
 
 func _process(delta: float) -> void:
-	pass
+	#print("This happens at every moment of game.")
+	var mouse_pos = get_global_mouse_position()
+	position.x = mouse_pos.x
+	
+	if position.x <= 0:
+		position.x = 0
+	
+	if position.x >= 480:
+		position.x = 480
